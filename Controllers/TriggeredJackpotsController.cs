5f2e5b84ee8b434032a0749d81c74e956c0f94cd
lcf007 <lcf007@msn.com>
2019-04-08 11:31:11 -0400

    Add Triggered Jackpots.

---
+++ Controllers/TriggeredJackpotsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebJackpot.Data;
+using WebJackpot.Models;
+
+namespace WebJackpot.Controllers
+{
+    public class TriggeredJackpotsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TriggeredJackpotsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TriggeredJackpots
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.TriggeredJackpots.Include(t => t.Jackpot);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: TriggeredJackpots/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var triggeredJackpot = await _context.TriggeredJackpots
+                .Include(t => t.Jackpot)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (triggeredJackpot == null)
+            {
+                return NotFound();
+            }
+
+            return View(triggeredJackpot);
+        }
+
+        // GET: TriggeredJackpots/Create
+        public IActionResult Create()
+        {
+            ViewData["JackpotID"] = new SelectList(_context.Jackpots, "JackpotID", "Name");
+            return View();
+        }
+
+        // POST: TriggeredJackpots/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,JackpotID,UserId,CurrentWin,TriggerTime")] TriggeredJackpot triggeredJackpot)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(triggeredJackpot);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JackpotID"] = new SelectList(_context.Jackpots, "JackpotID", "Name", triggeredJackpot.JackpotID);
+            return View(triggeredJackpot);
+        }
+
+        // GET: TriggeredJackpots/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var triggeredJackpot = await _context.TriggeredJackpots.FindAsync(id);
+            if (triggeredJackpot == null)
+            {
+                return NotFound();
+            }
+            ViewData["JackpotID"] = new SelectList(_context.Jackpots, "JackpotID", "Name", triggeredJackpot.JackpotID);
+            return View(triggeredJackpot);
+        }
+
+        // POST: TriggeredJackpots/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,JackpotID,UserId,CurrentWin,TriggerTime")] TriggeredJackpot triggeredJackpot)
+        {
+            if (id != triggeredJackpot.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(triggeredJackpot);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TriggeredJackpotExists(triggeredJackpot.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JackpotID"] = new SelectList(_context.Jackpots, "JackpotID", "Name", triggeredJackpot.JackpotID);
+            return View(triggeredJackpot);
+        }
+
+        // GET: TriggeredJackpots/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var triggeredJackpot = await _context.TriggeredJackpots
+                .Include(t => t.Jackpot)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (triggeredJackpot == null)
+            {
+                return NotFound();
+            }
+
+            return View(triggeredJackpot);
+        }
+
+        // POST: TriggeredJackpots/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var triggeredJackpot = await _context.TriggeredJackpots.FindAsync(id);
+            _context.TriggeredJackpots.Remove(triggeredJackpot);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TriggeredJackpotExists(int id)
+        {
+            return _context.TriggeredJackpots.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ ViewModels/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebJackpot.ViewModels
+{
+    public class Player
+    {
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal CurrentWin { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal TriggerPoints { get; set; }
+
+        [Display(Name = "Current Date")]
+        [DataType(DataType.DateTime)]
+        public DateTime CurrentTime { get; set; }
+    }
+
+}
+}
